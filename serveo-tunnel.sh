d454a3d1ed7c8f4506e8dc4d287bb9cc5f5b4d0a
A7med-Hosam <kingahmedhosam@gmail.com>
2026-03-19 17:35:43 +0200

    undo all ssh stuff

--- serveo-tunnel.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-if [ ! -f serveo_key ]; then
-    ssh-keygen -t rsa -b 2048 -f serveo_key -q -N ''
-fi
-
-autossh -M 0 -o 'StrictHostKeyChecking=no' -o 'ServerAliveInterval=30' -i serveo_key -R 9router-api:80:localhost:20128 serveo.net

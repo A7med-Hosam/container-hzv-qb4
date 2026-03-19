d0d0eefe844d4285d0e8e56e257acd33ed9fd630
A7med-Hosam <kingahmedhosam@gmail.com>
2026-03-19 17:46:12 +0200

    now the 5000 is running all that is left is 9router

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,3 +1,9 @@
 #!/bin/sh
 
+# Start 9router in the background
+echo "Starting 9router in the background..."
+9router &
+
+# Start the Node.js application in the foreground
+echo "Starting Node.js application..."
 exec node index.js
